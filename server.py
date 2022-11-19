9d9f6500c4cc0f54f3c88e6a6e030fe96fd22afb
Aldy Syachranie <aldysyach17@gmail.com>
2022-11-19 21:13:43 +0700

    Tambah gitignore dan web api untuk notify email

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, Response, request
+import pymongo
+import json
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host="mongodb://localhost:27017",
+        serverSelectionTimeoutMS = 3000
+    )
+    db = mongo.bicara_ai
+    mongo.server_info()
+except:
+    print("ERROR - Cannot connect to database")
+
+@app.route('/notifyme', methods=['POST'])
+def notify_user():
+    try:
+        user = {"email": request.form["email"]}
+        dbResponse = db.users.insert_one(user)
+        print(dbResponse.inserted_id)
+        return Response(response = json.dumps({"message": "Your email has been registered", "id": f"{dbResponse.inserted_id}"}), status = 200, mimetype="application/json")
+    except Exception as e:
+        return Response(json.dumps({"message": "Sorry, your email can't be registered"}), mimetype="application/json", status=500)
+
+if __name__ == '__main__':
+    app.run(debug=True)
