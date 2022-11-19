e76b84660bcd6d957a087424721c187265294b65
Fathan Bainal Kaffi <89505702+fathanbk@users.noreply.github.com>
2022-11-19 21:19:51 +0700

    Tambah Web API Service untuk sign up

--- server.py
+++ server.py
@@ -24,6 +24,26 @@ def notify_user():
         return Response(response = json.dumps({"message": "Your email has been registered", "id": f"{dbResponse.inserted_id}"}), status = 200, mimetype="application/json")
     except Exception as e:
         return Response(json.dumps({"message": "Sorry, your email can't be registered"}), mimetype="application/json", status=500)
+    
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        data = {
+            'email': request.form["email"], 
+            'password': request.form["password"],
+            'firstName': request.form["firstName"],
+            'lastName': request.form["lastName"],}
+        dbResponse = db.users.insert_one(data)
+        response = make_response(
+                jsonify(
+                    {"message": "User created successfully"}
+                ),
+                200,
+            )
+        response.headers["Content-Type"] = "application/json"
+        return response
+    except Exception as e: 
+        return jsonify({"error":str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
