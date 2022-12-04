4a4fa69f05b4f38cf75f6ce0c5d31913dc618cda
Aldy Syachranie <aldysyach17@gmail.com>
2022-12-04 20:48:44 +0700

    Add fps to videowriter and refactor code

--- AI/bicara.py
+++ AI/bicara.py
@@ -5,28 +5,32 @@ gaze = GazeTracking()
 video = cv2.VideoCapture('input.mp4')
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-output = cv2.VideoWriter('testreso.mp4',cv2.VideoWriter_fourcc('m','p','4','v'), 10, (width,height))
+fps = int(video.get(cv2.CAP_PROP_FPS))
+output = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
 
-while True:
-    _, frame = video.read()
+while video.isOpened():
+    ret, frame = video.read()
 
-    gaze.refresh(frame)
+    if ret == True:
+        gaze.refresh(frame)
 
-    frame = gaze.annotated_frame()
-    text = ""
+        frame = gaze.annotated_frame()
+        text = ""
 
-    if gaze.is_right():
-        text = "=> right"
-    elif gaze.is_left():
-        text = "left <="
-    elif gaze.is_center():
-        text = "=center="
+        if gaze.is_right():
+            text = "=> right"
+        elif gaze.is_left():
+            text = "left <="
+        elif gaze.is_center():
+            text = "=center="
 
-    cv2.putText(frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    cv2.imshow("Eye contact detection", frame)
-    output.write(frame)
+        cv2.putText(frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+        cv2.imshow("Eye contact detection", frame)
+        output.write(frame)
 
-    if cv2.waitKey(1) == 27:
+        if cv2.waitKey(1) == 27:
+            break
+    else:
         break
 
 video.release()
