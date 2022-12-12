487820427f3c08cb96548403178bc7d5417b1ef4
fathanbk <fathanbk08@gmail.com>
2022-12-12 17:30:41 +0700

    updated speech-to-text

--- Video Processing/speech_to_text.py
+++ Video Processing/speech_to_text.py
@@ -1,23 +1,19 @@
-# Step 1 : Importing libararies
 import speech_recognition as sr 
 import moviepy.editor as mp
 
-# Step 2: Video to Audio conversion
 
-VidClip = mp.VideoFileClip("/content/data/video.mp4") 
-VidClip.audio.write_audiofile("/content/data/converted.wav")
 
-# Step 3: Speech recognition
+VidClip = mp.VideoFileClip("final.mp4") 
+VidClip.audio.write_audiofile("converted.wav")
+
 
 reco = sr.Recognizer()
-audio = sr.AudioFile("/content/data/converted.wav")
+audio = sr.AudioFile("converted.wav")
 with audio as source:
   audio_file = reco.record(source)
-result = reco.recognize_google(audio_file)
+result = reco.recognize_google(audio_file, language = 'id')
 
-# Step 4: Finally exporting the result 
-
-with open('/content/data/SpeechText.txt',mode ='w') as file: 
+with open('SpeechText.txt',mode ='w') as file: 
    file.write("Recognized Speech Text:") 
    file.write("\n") 
    file.write(result) 
